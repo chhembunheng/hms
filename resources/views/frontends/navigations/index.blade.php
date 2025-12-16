91a06ac5e511263ccf4f5972fef92734c2d6363d
Hengslytherinn <143009767+chhembunheng@users.noreply.github.com>
2025-12-16 21:51:08 +0700

    bugs

--- resources/views/frontends/navigations/index.blade.php
+++
@@ -1,6 +0,0 @@
-<x-app-layout>
-    <div class="content">
-        <x-datatables title="{{ __('root.common.navigations') }}" :data="$dataTable">
-        </x-datatables>
-    </div>
-</x-app-layout>
--- resources/views/frontends/products/partials/templates/detail-template.blade.php
+++
@@ -1,8 +0,0 @@
-<script type="text/template" id="detailTemplate">
-{!! str_replace("\n", "", view('frontends.products.partials.detail', [
-    'featureIndex' => '__FEATURE_INDEX__',
-    'detailIndex' => '__DETAIL_INDEX__',
-    'detail' => ['translations' => []],
-    'locales' => $locales
-])->render()) !!}
-</script>
